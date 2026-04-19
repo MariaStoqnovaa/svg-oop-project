b203ff4fa4ca53abf1b19484538f2b0b2b5c258b
MariaStoqnovaa <122725571+MariaStoqnovaa@users.noreply.github.com>
2026-04-19 15:02:21 +0300

    Commands and new changes

--- SVG_OOP_Project/src/Models/Rectangle.java
+++ SVG_OOP_Project/src/Models/Rectangle.java
@@ -1,67 +1,70 @@
 package Models;
 
-public class Rectangle extends Shape
-{
+public class Rectangle extends Shape {
 
-    private int a;
-    private int b;
-    private String colorFill;
+    private double width;
+    private double height;
+    private String fill; // color
 
-    public Rectangle(double x, double y, int a, int b, String colorFill) {
+    public Rectangle(double x, double y, double width, double height, String fill) {
         super(x, y);
-        this.a = a;
-        this.b = b;
-        this.colorFill = colorFill;
+        this.width = width;
+        this.height = height;
+        this.fill = fill;
     }
 
-    public int getA() {
-        return a;
+    public double getWidth() {
+        return width;
     }
 
-    public void setA(int a) {
-        this.a = a;
+    public double getHeight() {
+        return height;
     }
 
-    public int getB() {
-        return b;
+    public String getFill() {
+        return fill;
     }
 
-    public void setB(int b) {
-        this.b = b;
-    }
+    public double getA() { return width; }
+    public double getB() { return height; }
 
     @Override
-    boolean isInRegion(Shape region)
-    {
-        if(region instanceof Rectangle rect)
-        {
-            return this.x >= rect.getX() &&
-                    this.y >= rect.getY() &&
-                    (this.x + this.a) <= (rect.getX() + rect.getA()) &&
-                    (this.y + this.b) <= (rect.getY() + rect.getB());
+    public boolean isInRegion(Shape region) {
+        if (region instanceof Rectangle) {
+            Rectangle rect = (Rectangle) region;
+            boolean leftOk  = getX() >= rect.getX();
+            boolean topOk   = getY() >= rect.getY();
+            boolean rightOk = (getX() + width)  <= (rect.getX() + rect.getWidth());
+            boolean botOk   = (getY() + height) <= (rect.getY() + rect.getHeight());
+            return leftOk && topOk && rightOk && botOk;
         }
-        else if(!(region instanceof Circle circ))
-        {
-            return false;
+
+        if (region instanceof Circle) {
+            Circle circ = (Circle) region;
+
+            return pointInCircle(getX(),         getY(),          circ)
+                && pointInCircle(getX() + width, getY(),          circ)
+                && pointInCircle(getX(),         getY() + height, circ)
+                && pointInCircle(getX() + width, getY() + height, circ);
         }
-        else
-        {
-            return this.circleIsPoint(this.x, this.y, circ) &&
-                    this.circleIsPoint(this.x + this.a, this.y, circ) &&
-                    this.circleIsPoint(this.x, this.y + this.b, circ) &&
-                    this.circleIsPoint(this.x + this.a, this.y + this.b, circ);
-        }
+
+        return false;
     }
 
-    private boolean circleIsPoint(double px,double py,Circle c)
-    {
+    private boolean pointInCircle(double px, double py, Circle c) {
         double dx = px - c.getX();
         double dy = py - c.getY();
         return dx * dx + dy * dy <= c.getRadius() * c.getRadius();
     }
-    @Override
-    String toSVG() {
-        return "<rectangle center X= " + this.x + ",center Y= " + this.y + ",width= " + this.a +",height: "+ this.b+ ",color fill = " + colorFill;
 
+    @Override
+    public String toSVG() {
+        return String.format("<rect x=\"%.1f\" y=\"%.1f\" width=\"%.1f\" height=\"%.1f\" fill=\"%s\" />",
+                getX(), getY(), width, height, fill);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("rectangle %.1f %.1f %.1f %.1f %s", getX(), getY(), width, height, fill);
     }
 }
--- SVG_OOP_Project/src/Models/Shape.java
+++ SVG_OOP_Project/src/Models/Shape.java
@@ -2,35 +2,27 @@ package Models;
 
 public abstract class Shape {
 
-    protected double x;
-    protected double y;
+    protected Point position;
 
     public Shape(double x, double y) {
-        this.x = x;
-        this.y = y;
+        this.position = new Point(x, y);
     }
 
     public double getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
+        return position.getX();
     }
 
     public double getY() {
-        return y;
+        return position.getY();
     }
 
-    public void setY(int y) {
-        this.y = y;
+    public void translate(double horizontal, double vertical) {
+        position.translate(horizontal, vertical);
     }
 
-    public void translate(double x, double y) {
-        this.x = x;
-        this.y = y;
-    }
+    public abstract boolean isInRegion(Shape region);
 
-    abstract boolean isInRegion(Shape region);
-    abstract String toSVG();
+    public abstract String toSVG();
+
+    public abstract String toString();
 }
